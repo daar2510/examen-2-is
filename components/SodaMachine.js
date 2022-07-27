5c3f748e41e7d05528b488c0efc2132002c3dad4
David Atias <datias@akurey.com>
2022-07-27 15:08:01 -0600

    Added purchase preview functionality

--- components/SodaMachine.js
+++ components/SodaMachine.js
@@ -3,12 +3,18 @@ import styles from "/styles/SodaMachine.module.css";
 import Image from "next/image";
 import SodaMachineScreen from "./SodaMachineScreen";
 import { inventory, soda1, soda2, soda3, soda4 } from "../constants/inventory";
+import { titles } from "../constants/text";
 import MoneyReader from "./MoneyAcceptor";
 import SodaButton from "./SodaButton";
+import ErrorAlert from "./ErrorAlert";
+import { calculatePrice, calculateTotal } from "../logic/soda";
 
 const SodaMachine = () => {
   const [isZoomEnabled, setIsZoomEnabled] = useState(false);
   const [inventoryState, setInventoryState] = useState(inventory);
+  const [purchaseStarted, setPurchaseStarted] = useState(false);
+  const [sodaSelection, setSodaSelection] = useState([]);
+  const [error, setError] = useState(false);
 
   const handleKeyDown = useCallback(
     (event) => {
@@ -19,6 +25,46 @@ const SodaMachine = () => {
     [isZoomEnabled]
   );
 
+  const handleSodaButtonClick = (sodaName) => {
+    setPurchaseStarted(true);
+    const newSodaSelection = [...sodaSelection];
+    newSodaSelection.pop();
+    
+    const sodaIndex = newSodaSelection.findIndex((soda) =>
+      soda.sodaName.includes(sodaName)
+    );
+    const inventoryIndex = inventoryState.findIndex((item) =>
+      item.sodaName.includes(sodaName)
+    );
+    if (sodaIndex !== -1) {
+      const newQuantity =
+        parseInt(sodaSelection[sodaIndex].sodaName.split(" ")[0]) + 1;
+      newSodaSelection[sodaIndex].sodaName = `${newQuantity} ${sodaName}`;
+      newSodaSelection[sodaIndex].quantity = calculatePrice(
+        sodaName,
+        newQuantity
+      );
+      
+      if (newQuantity > inventoryState[inventoryIndex].quantity) {
+        setError(true);
+        newSodaSelection.splice(sodaIndex, 1);
+        if (newSodaSelection.length === 0){
+          setPurchaseStarted(false);
+        }
+        setTimeout(() => {
+          setError(false);
+        }, 3000);
+      }
+    } else {
+      newSodaSelection.push({
+        sodaName: `${1} ${sodaName}`,
+        quantity: calculatePrice(sodaName, 1),
+      });
+    }
+    newSodaSelection.push({sodaName: "total", quantity: calculateTotal(newSodaSelection)});
+    setSodaSelection(newSodaSelection);
+  };
+
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     return () => {
@@ -28,6 +74,7 @@ const SodaMachine = () => {
 
   return (
     <>
+      <ErrorAlert error={titles.quantityError} isVisible={error} />
       <div
         className={`${styles["soda-machine"]} ${
           isZoomEnabled ? styles["zoom-enabled"] : styles["zoom-disabled"]
@@ -43,13 +90,36 @@ const SodaMachine = () => {
         </div>
         <div className={styles.divider} />
         <div className={styles["right-section"]}>
-          <SodaMachineScreen inventory={inventoryState} />
+          <SodaMachineScreen
+            title={purchaseStarted ? titles.purchase : titles.inventory}
+            inventory={purchaseStarted ? sodaSelection : inventoryState}
+          />
           <MoneyReader />
           <div className={styles["btn-group"]}>
-            <SodaButton {...soda1} />
-            <SodaButton {...soda2} />
-            <SodaButton {...soda3} />
-            <SodaButton {...soda4} />
+            <SodaButton
+              {...soda1}
+              onClick={() => {
+                handleSodaButtonClick(inventoryState[0].sodaName);
+              }}
+            />
+            <SodaButton
+              {...soda2}
+              onClick={() => {
+                handleSodaButtonClick(inventoryState[1].sodaName);
+              }}
+            />
+            <SodaButton
+              {...soda3}
+              onClick={() => {
+                handleSodaButtonClick(inventoryState[2].sodaName);
+              }}
+            />
+            <SodaButton
+              {...soda4}
+              onClick={() => {
+                handleSodaButtonClick(inventoryState[3].sodaName);
+              }}
+            />
           </div>
         </div>
       </div>
