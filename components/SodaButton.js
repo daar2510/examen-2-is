65d777accb49381d81eff11156b6296e0bdf2245
David Atias <datias@akurey.com>
2022-07-27 15:06:59 -0600

    soda buttons now receive a onclick callback

--- components/SodaButton.js
+++ components/SodaButton.js
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import styles from "/styles/SodaButton.module.css";
 
-const SodaButton = ({ soda, price }) => {
+const SodaButton = ({ soda, price, onClick }) => {
   return (
-    <button className={styles["soda-btn"]}>
+    <button className={styles["soda-btn"]} onClick={onClick}>
       <div className={`${styles["soda-img"]} ${styles[soda]}`}>
         <div className={styles["price-tag"]}>
           <p className={styles["price"]}>{`¢${price}`}</p>
