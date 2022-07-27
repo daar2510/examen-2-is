ce10ad910b77f05133bbf52bbbe367d150aa942d
David Atias <davidaar2000@gmail.com>
2022-07-27 16:51:08 -0600

    added Menu to page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import SodaMachine from "../components/SodaMachine";
 import styles from "/styles/Home.module.css";
 import { titles } from "../constants/text";
+import { useState } from "react";
+import Menu from "../components/Menu";
 
 export default function Home() {
+  const [total, setTotal] = useState(1);
   return (
     <div className={styles.main}>
-      <SodaMachine />
-      <h1 className={styles.title}>{titles.homeTitle}</h1>
+      <SodaMachine total={total} setTotal={setTotal} />
+      <div>
+        <h1 className={styles.title}>{titles.homeTitle}</h1>
+        <Menu total={total} setTotal={setTotal} />
+      </div>
     </div>
   );
 }
