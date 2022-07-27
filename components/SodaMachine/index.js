344f4e5f44e2cae86dc26fc794d0525843ecd6c4
David Atias <datias@akurey.com>
2022-07-27 00:31:44 -0600

    Added soda machine body and banner

--- components/SodaMachine/index.js
+++ components/SodaMachine/index.js
@@ -1,13 +1,21 @@
 import SodaButton from "../SodaButton";
 import styles from "../../styles/SodaMachine.module.css";
+import Image from "next/image";
 
 const SodaMachine = () => {
     return (
         <>
-            <SodaButton className={styles["soda-machine"]} soda={"coca"} />
-            <SodaButton className={styles["soda-machine"]} soda={"sprite"} />
-            <SodaButton className={styles["soda-machine"]} soda={"fanta"} />
-            <SodaButton className={styles["soda-machine"]} soda={"pepsi"} />
+            <div className={styles["soda-machine"]}>
+                <div className={styles.banner}>
+                    <Image
+                        src="/assets/coca-bg.png"
+                        alt="Soda Machine Banner"
+                        width={362}
+                        height={816}
+                    />
+                </div>
+                <div className={styles.divider} />
+            </div>
         </>
     );
 };
