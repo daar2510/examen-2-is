a59d7b5b3e36f4c63d5279c4f71372f02a369483
David Atias <datias@akurey.com>
2022-07-27 15:05:57 -0600

    added new constant strings

--- constants/text.js
+++ constants/text.js
@@ -1,4 +1,6 @@
 export const titles = {
   homeTitle: "MÁQUINA EXPENDEDORA DE REFRESCOS",
   inventory: "Inventario",
+  purchase: "Tu Compra",
+  quantityError: "No hay suficientes unidades de este producto, intenta de nuevo",
 };
