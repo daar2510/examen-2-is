c6759ae631b2334db6392ef1391b2e1a0fa08d91
David Atias <davidaar2000@gmail.com>
2022-07-27 16:42:55 -0600

    Added Money component

---
+++ components/Money.js
@@ -0,0 +1,23 @@
+import styles from "/styles/Money.module.css";
+import PropTypes from "prop-types";
+
+const Money = ({ value, total, setTotal }) => {
+  return (
+    <div
+      className={styles.coin}
+      onClick={() => {
+        if (total > 1) setTotal(total - value);
+      }}
+    >
+      <p className={styles["money-text"]}>{value}</p>
+    </div>
+  );
+};
+
+Money.propTypes = {
+  value: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  setTotal: PropTypes.func.isRequired,
+}
+
+export default Money;
