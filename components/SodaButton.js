78fa2effbb827caff1ca8d9eedcab13d7fddb3cb
David Atias <datias@akurey.com>
2022-07-27 15:58:26 -0600

    removed buttons pressed with space key

--- components/SodaButton.js
+++ components/SodaButton.js
@@ -2,8 +2,14 @@ import PropTypes from "prop-types";
 import styles from "/styles/SodaButton.module.css";
 
 const SodaButton = ({ soda, price, onClick }) => {
+  const handleKeyDown = (e) => {
+    if (e.key === " ") {
+      e.preventDefault();
+    }
+  }
+  
   return (
-    <button className={styles["soda-btn"]} onClick={onClick}>
+    <button className={styles["soda-btn"]} onClick={onClick} onKeyDown={handleKeyDown}>
       <div className={`${styles["soda-img"]} ${styles[soda]}`}>
         <div className={styles["price-tag"]}>
           <p className={styles["price"]}>{`¢${price}`}</p>
