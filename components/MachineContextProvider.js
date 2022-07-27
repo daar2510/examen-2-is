e6936443c2f4ed06a5ae2ebc41c1872dedaabaff
David Atias <davidaar2000@gmail.com>
2022-07-27 17:01:31 -0600

    created a machine context to manage state

---
+++ components/MachineContextProvider.js
@@ -0,0 +1,51 @@
+import { useContext, useState, createContext } from "react";
+import { sodas } from "/data/sodas";
+import { coins } from "/data/coins";
+
+const MachineContext = createContext();
+
+export const useMachineContext = () => useContext(MachineContext);
+
+const MachineContextProvider = ({ children }) => {
+  const [isZoomEnabled, setIsZoomEnabled] = useState(false);
+  const [inventory, setInventory] = useState(sodas);
+  const [hasPurchaseStarted, setHasPurchaseStarted] = useState(false);
+  const [userSodaSelection, setUserSodaSelection] = useState([]);
+  const [quantityError, setQuantityError] = useState(false);
+  const [noCoinsError, setNoCoinsError] = useState(false);
+  const [noChangeError, setNoChangeError] = useState(false);
+  const [machineCoins, setMachineCoins] = useState(coins);
+  const [changeCoins, setChangeCoins] = useState([]);
+  const [total, setTotal] = useState(1);
+
+  const machineContext = {
+    isZoomEnabled,
+    setIsZoomEnabled,
+    inventory,
+    setInventory,
+    hasPurchaseStarted,
+    setHasPurchaseStarted,
+    userSodaSelection,
+    setUserSodaSelection,
+    quantityError,
+    setQuantityError,
+    noCoinsError,
+    setNoCoinsError,
+    noChangeError,
+    setNoChangeError,
+    machineCoins,
+    setMachineCoins,
+    changeCoins,
+    setChangeCoins,
+    total,
+    setTotal,
+  };
+
+  return (
+    <MachineContext.Provider value={machineContext}>
+      {children}
+    </MachineContext.Provider>
+  );
+};
+
+export default MachineContextProvider;
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,19 @@ import styles from "/styles/Home.module.css";
 import { titles } from "../constants/text";
 import { useState } from "react";
 import Menu from "../components/Menu";
+import MachineContextProvider from "../components/MachineContextProvider";
 
 export default function Home() {
   const [total, setTotal] = useState(1);
   return (
-    <div className={styles.main}>
-      <SodaMachine total={total} setTotal={setTotal} />
-      <div>
-        <h1 className={styles.title}>{titles.homeTitle}</h1>
-        <Menu total={total} setTotal={setTotal} />
+    <MachineContextProvider>
+      <div className={styles.main}>
+        <SodaMachine total={total} setTotal={setTotal} />
+        <div>
+          <h1 className={styles.title}>{titles.homeTitle}</h1>
+          <Menu total={total} setTotal={setTotal} />
+        </div>
       </div>
-    </div>
+    </MachineContextProvider>
   );
 }
