a25c48bd02ae2275114fbc97e9c9f301f155a627
David Atias <davidaar2000@gmail.com>
2022-07-27 17:19:16 -0600

    Added new text constants

--- constants/text.js
+++ constants/text.js
@@ -2,6 +2,9 @@ export const titles = {
   homeTitle: "MÁQUINA EXPENDEDORA DE REFRESCOS",
   inventory: "Inventario",
   purchase: "Tu Compra",
+  money: "MI DINERO",
+  tutorial: "VER TUTORIAL",
+  settings: "AJUSTES",
   quantityError:
     "No hay suficientes unidades de este producto, intenta de nuevo",
 };
