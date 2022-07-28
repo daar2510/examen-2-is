0e645d23daec6639ce05bf0623dec6de4078a2a0
David Atias <davidaar2000@gmail.com>
2022-07-27 21:58:20 -0600

    added change breakdown screen

--- components/SodaMachineScreen.js
+++ components/SodaMachineScreen.js
@@ -1,13 +1,15 @@
 import styles from "/styles/SodaMachineScreen.module.css";
 import { useMachineContext } from "./MachineContextProvider";
-import { calculatePrice } from "../logic/soda";
+import { calculatePrice, sumAllCoins } from "../logic/soda";
 import { titles } from "/constants/text";
 
 const SodaMachineScreen = () => {
+  const { areSodasDispensed } = useMachineContext();
   const { hasPurchaseStarted } = useMachineContext();
   const { userSodaSelection } = useMachineContext();
   const { inventory } = useMachineContext();
   const { total } = useMachineContext();
+  const { changeCoins } = useMachineContext();
 
   const renderUserSodaSelection = () => {
     const selectionFields = userSodaSelection.map((selection) => {
@@ -49,8 +51,50 @@ const SodaMachineScreen = () => {
     );
   };
 
+  const renderChange = () => {
+    const changeFields = changeCoins.map((coin) => {
+      return (
+        <div className={styles.field} key={coin.value}>
+          <p>{`${coin.quantity} moneda${coin.quantity > 1 ? "s" : ""} de ${
+            coin.value
+          }`}</p>
+        </div>
+      );
+    });
+    return (
+      <>
+        <div className={`${styles.field} ${styles["extra-space"]}`}>
+          <p>{`${titles.change} ${sumAllCoins(changeCoins)} ${
+            titles.currencyName
+          }`}</p>
+        </div>
+        <div className={`${styles.field} ${styles["extra-space"]}`}>
+          <p>{titles.breakdown}</p>
+        </div>
+        {changeFields}
+      </>
+    );
+  };
+
+  const renderCollectSoda = () => {
+    return (
+      <>
+        <p className={`${styles.title} ${styles["extra-space"]}`}>
+          {titles.thanks}
+        </p>
+        <div className={`${styles.field} ${styles["extra-space"]}`}>
+          <p>{titles.collectSoda}</p>
+        </div>
+      </>
+    );
+  };
+
   const renderText = () => {
-    if (hasPurchaseStarted) {
+    if (changeCoins.length > 0) {
+      return renderChange();
+    } else if (areSodasDispensed) {
+      return renderCollectSoda();
+    } else if (hasPurchaseStarted) {
       return renderUserSodaSelection();
     } else {
       return renderInventory();
