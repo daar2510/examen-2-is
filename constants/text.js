d4684b290521ed973e111dc247b7d540896a803f
David Atias <davidaar2000@gmail.com>
2022-07-27 22:10:29 -0600

    new constants

--- constants/text.js
+++ constants/text.js
@@ -8,6 +8,7 @@ export const titles = {
   settings: "AJUSTES",
   quantityError:
     "No hay suficientes unidades de este producto, intenta de nuevo",
+  noChangeError: "La maquina no tiene suficientes monedas\n Intenta pagar con otras monedas o la cantidad exacta",
   change: "su vuelto es de",
   breakdown: "desglose:",
   currency: "¢",
--- constants/time.js
+++ constants/time.js
@@ -1,4 +1,4 @@
 export const milisecondsPerSecond = 1000;
 export const quantityErrorTimeout = milisecondsPerSecond * 3;
-export const noChangeErrorTimeout = milisecondsPerSecond * 6;
+export const noChangeErrorTimeout = milisecondsPerSecond * 7;
 export const updateSodasDispensedTimeout = 300;
