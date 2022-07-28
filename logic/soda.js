dfe3ee209110fc6ce145c61b05f5b43f27170619
David Atias <davidaar2000@gmail.com>
2022-07-27 18:19:16 -0600

    soda selection Logic and updated previous logic

--- logic/soda.js
+++ logic/soda.js
@@ -1,13 +1,25 @@
-import { soda1, soda2, soda3, soda4 } from "../constants/inventory";
-const sodaOptions = [soda1, soda2, soda3, soda4];
+import { sodas } from "/data/sodas";
 
 export const calculatePrice = (sodaName, quantity) => {
-  const soda = sodaOptions.find((item) => item.soda === sodaName);
+  const soda = sodas.find((soda) => soda.name === sodaName);
   return soda.price * quantity;
 };
 
-export const calculateTotal = (sodaSelection) => {
-  return sodaSelection.reduce((total, item) => {
-    return total + item.quantity;
+export const calculateTotal = (userSodaSelection) => {
+  return userSodaSelection.reduce((total, selection) => {
+    return total + calculatePrice(selection.soda, selection.quantity);
   }, 0);
+};
+
+export const addSodaToCurrentSelection = (userSodaSelection, sodaToAdd) => {
+  const newUserSodaSelection = [...userSodaSelection];
+  const index = newUserSodaSelection.findIndex(
+    (selection) => selection.soda === sodaToAdd
+  );
+  if (newUserSodaSelection.length === 0 || index === -1) {
+    newUserSodaSelection.push({ soda: sodaToAdd, quantity: 1 });
+  } else {
+    newUserSodaSelection[index].quantity++;
+  }
+  return newUserSodaSelection;
 };
