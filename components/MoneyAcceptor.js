6e8056574e244e4254280a467e4df5d98c648b8d
David Atias <davidaar2000@gmail.com>
2022-07-27 21:37:06 -0600

    Added change functionality

--- components/MoneyAcceptor.js
+++ components/MoneyAcceptor.js
@@ -1,12 +1,59 @@
 import styles from "/styles/MoneyAcceptor.module.css";
+import { useMachineContext } from "./MachineContextProvider";
+import { useEffect } from "react";
+import { calculateChangeCoins } from "/logic/soda";
+import { noChangeErrorTimeout } from "/constants/time";
+import Image from "next/image";
 
 const MoneyAcceptor = () => {
+  const { changeCoins, setChangeCoins } = useMachineContext();
+  const { machineCoins, setMachineCoins } = useMachineContext();
+  const { setHasPurchaseStarted, setAreSodasDispensed, setUserSodaSelection } =
+    useMachineContext();
+  const { total, setTotal } = useMachineContext();
+  const { setNoChangeError } = useMachineContext();
+
+  useEffect(() => {
+    if (total < 0) {
+      const { change, remaingAmountToReturn, remainingCoins } =
+        calculateChangeCoins(-total, machineCoins);
+      if (remaingAmountToReturn > 0) {
+        setNoChangeError(true);
+        setAreSodasDispensed(false);
+        setHasPurchaseStarted(false);
+        setUserSodaSelection([]);
+        setTotal(0);
+
+        setTimeout(() => {
+          setNoChangeError(false);
+        }, noChangeErrorTimeout);
+      } else {
+        setChangeCoins(change);
+        setMachineCoins(remainingCoins);
+      }
+    }
+  }, [total]);
+
   return (
     <div className={styles.container}>
       <div className={styles["money-acceptor"]}>
         <div className={styles["bill-slot"]} />
       </div>
-      <div className={styles["coin-slot"]} />
+      <div className={styles["coin-slot"]}>
+        {changeCoins.length > 0 && (
+          <div className={styles["coin-pile"]}>
+            <Image
+              src="/assets/coin-pile.png"
+              alt="coin pile"
+              width={50}
+              height={20}
+              onClick={() => {
+                setChangeCoins([]);
+              }}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
